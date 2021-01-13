b5f26437c0a7d4e663f1947bfb4609cbe1855c9c
keonlee9420 <keonlee9420@gmail.com>
2021-01-14 00:14:28 +0900

    git track bc13_global_mean.npy

--- hparams.py
+++ hparams.py
@@ -98,7 +98,7 @@ def create_hparams(hparams_string=None, verbose=False):
         use_mmi=False,
         use_gaf=False,
         max_gaf=0.5,
-        global_mean_npy='datasets/bc13_global_mean.npy'
+        global_mean_npy='filelists/bc13_global_mean.npy'
     )
 
     if hparams_string:
